30b046f07aa94d2986f0209f7f1923e314522cb6
zadewg <38017709+zadewg@users.noreply.github.com>
2018-08-15 20:18:05 +0200

    Update ASYNC.py

--- ASYNC.py
+++ ASYNC.py
@@ -1,16 +1,27 @@
-def resample():
-	pass
+class Sync:
+	
+	def _init__(self, x, y):
+		self.x = x
+		self.y = y
 
-def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+	def _isclose(self, a, b, rel_tol=1e-09, abs_tol=0.0):
+		return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
-def check(data)
+	def check(self, x):
+	
+		i, f = 0
+		while i < (len(x) - 1):
+			if i > 1:
+				a = (float(times[i]) - float(times[i - 1]))
+				b = (float(times[i + 1]) - float(times[i]))
+				if not (self.isclose(a, b)):
+					f += 1
+					
+			i += 1
+		if f:
+			return 'True'
+		else:
+			return 'False'
 
-	i = 0
-	while i < (len(data) - 1):
-		if i > 1:
-			a = (float(times[i]) - float(times[i - 1]))
-			b = (float(times[i + 1]) - float(times[i]))
-			if not (isclose(a, b)):
-				resample()
-		i += 1
+	def resample(self, x, y):
+		pass
